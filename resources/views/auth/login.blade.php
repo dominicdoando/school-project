c2e583cd3cd2dbdcf595ff2f2e1fb58e67b2cc9d
dominicdoando <dominicdoando@gmail.com>
2021-07-26 20:39:58 +0700

    finish admin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -45,4 +45,84 @@
             </div>
         </form>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+	@extends('layouts.master_admin')
+    @section('master_admin')
+    <div class="container h-p100">
+        <div class="row align-items-center justify-content-md-center h-p100">	
+            @if (session('status'))
+                <div class="mb-4 font-medium text-sm text-green-600">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <div class="col-12">
+                <div class="row justify-content-center no-gutters">
+                    <div class="col-lg-4 col-md-5 col-12">
+                        <div class="content-top-agile p-10">
+                            <h2 class="text-white">Get started with Us</h2>
+                            <p class="text-white-50">Sign in to start your session</p>							
+                        </div>
+                        <div class="p-30 rounded30 box-shadowed b-2 b-dashed">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text bg-transparent text-white"><i class="ti-user"></i></span>
+                                        </div>
+                                        <input type="email" class="form-control pl-15 bg-transparent text-white plc-white" placeholder="Email" type="email" name="email" :value="old('email')">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text  bg-transparent text-white"><i class="ti-lock"></i></span>
+                                        </div>
+                                        <input type="password" class="form-control pl-15 bg-transparent text-white plc-white" placeholder="Password" type="password" name="password" required autocomplete="current-password">
+                                    </div>
+                                </div>
+                                  <div class="row">
+                                    <div class="col-6">
+                                      <div class="checkbox text-white">
+                                        <input type="checkbox"  id="remember_me" name="remember">
+                                        <label for="remember_me">Remember Me</label>
+                                      </div>
+                                    </div>
+                                    <!-- /.col -->
+                                    @if (Route::has('password.request'))
+                                        <div class="col-6">
+                                            <div class="fog-pwd text-right">
+                                               <a href="{{ route('password.request') }}" class="text-white hover-info"><i class="ion ion-locked"></i> Forgot pwd?</a><br>
+                                             </div>
+                                        </div>
+                                    @endif
+                               
+                                    <!-- /.col -->
+                                    <div class="col-12 text-center">
+                                      <button type="submit" class="btn btn-info btn-rounded mt-10">SIGN IN</button>
+                                    </div>
+                                    <!-- /.col -->
+                                  </div>
+                            </form>														
+    
+                            <div class="text-center text-white">
+                              <p class="mt-20">- Sign With -</p>
+                              <p class="gap-items-2 mb-20">
+                                  <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-facebook"></i></a>
+                                  <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-twitter"></i></a>
+                                  <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-google-plus"></i></a>
+                                  <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-instagram"></i></a>
+                                </p>	
+                            </div>
+                            
+                            <div class="text-center">
+                                <p class="mt-15 mb-0 text-white">Don't have an account? <a href="{{ route('register') }}" class="text-info ml-5">Sign Up</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endsection
